f75e0e6369f31a61e92242d4b576953ada5d2819
Brent Champion <brent.champion@queensu.ca>
2021-04-24 15:19:07 -0400

    SL2 update unit tests

--- client/src/App.test.tsx
+++ client/src/App.test.tsx
@@ -1,9 +1,8 @@
-import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders the Sign In button', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText('Sign In');
   expect(linkElement).toBeInTheDocument();
 });
