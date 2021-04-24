73c630111ff80631d0d649657269e43be93692ef
Brent Champion <brent.champion@queensu.ca>
2021-04-24 18:02:57 -0400

    updates unit tests

--- client/src/App.test.tsx
+++ client/src/App.test.tsx
@@ -1,8 +1,27 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders the Sign In button', () => {
-  render(<App />);
-  const linkElement = screen.getByText('Sign In');
-  expect(linkElement).toBeInTheDocument();
+describe('Page Rendering Tests', () => {
+  beforeEach(() => {
+    render(<App />);
+  });
+
+  test('It renders the Spotify Logo', () => {
+    const logoElement = screen.getByAltText("logo");
+    expect(logoElement).toBeInTheDocument();
+  });
+
+  test('It renders the information text', () => {
+    const infoText: string = "A Spotify Authentication Lambda using Serverless";
+    const infoElement = screen.getByText(infoText);
+    expect(infoElement).toBeInTheDocument();
+  });
+
+
+  test('It renders the sign in button', () => {
+    const signInText: string = "Sign In";
+    const linkElement = screen.getByText(signInText);
+    expect(linkElement).toBeInTheDocument();
+  });
+  
 });
