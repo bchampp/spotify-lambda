88c055adf3bb1cc69faf3f108a815744e01bc1e0
Brent Champion <brent.champion@queensu.ca>
2021-04-24 22:24:55 -0400

    Update Lambda Deployment and Testing (#5)
    * update lambda
    
    * update workflows
    
    * updates label
    
    * updates key
    
    * add serverless framework to deployment
    
    * update prod scripts

---
+++ lambda/__tests__/handler.test.ts
@@ -0,0 +1,13 @@
+// import { login } from "../handler";
+
+// const handler = require('../handler');
+
+describe('Spotify Authorization Lambda Unit Tests', () => {
+    test('Login API', () => {
+        expect(1+1).toBe(2);
+    });
+
+    test('Callback API', () => {
+        expect(1+1).toBe(2);
+    });
+});
