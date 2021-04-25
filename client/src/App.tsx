cf68108ad9382df99379ecf9bc64f2afefe6b95c
Brent Champion <brent.champion@queensu.ca>
2021-04-25 19:45:59 -0400

    adds environment to prod deployment script (#7)
    * adds environment to prod deployment scriptg
    
    * adds environment to pr deployment scriptg

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,12 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+	const login_url = 'https://lp2ilg294l.execute-api.ca-central-1.amazonaws.com/prod/login'
+	console.log(login_url);
+
 	return (
 		<div className="App">
 			<header className="App-header">
 				<img src={logo} className="App-logo" alt="logo" />
 				<p>A Spotify Authentication Lambda using Serverless</p>
-				<a className="App-link" target="_blank" href="spotify.com">
+				<a className="App-link" href={login_url}>
 					Sign In
 				</a>
 			</header>
