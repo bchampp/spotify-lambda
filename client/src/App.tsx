ec4f6c14e584f9ea1505d633ee21e162bdb1d6d9
Brent Champion <brent.champion@queensu.ca>
2021-04-25 21:05:06 -0400

    updates client to conditionally render if signed in

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,35 @@
+import { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+const queryString = require('query-string');
 
 function App() {
 	const login_url = 'https://lp2ilg294l.execute-api.ca-central-1.amazonaws.com/prod/login';
 
+	const [isSignedIn, setIsSignedIn] = useState<boolean>(false);
+
+	useEffect(() => {
+		const s = queryString.parse(window.location.search);
+
+		if (s.access_token) {
+			window.history.replaceState({}, "Spotify Auth Lambda", "/"); // Change URL.
+			setIsSignedIn(true);
+		}		
+	}, []);
+
 	return (
 		<div className="App">
 			<header className="App-header">
+			<h3>A Spotify Authentication Lambda using Serverless</h3>
 				<img src={logo} className="App-logo" alt="logo" />
-				<p>A Spotify Authentication Lambda using Serverless</p>
-				<a className="App-link" href={login_url}>
-					Sign In
-				</a>
+				{
+					isSignedIn ? 
+					<p>Welcome back! You are now signed in.</p>
+					:
+					<a className="App-link" href={login_url}>
+						Sign In
+					</a>
+				}
 			</header>
 		</div>
 	);
